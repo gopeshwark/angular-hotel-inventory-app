98215c8513fbaa477fff1d9da519457c84266d15
gopeshwark <gopeshwark@gmail.com>
2023-03-02 00:15:40 +0530

    Created Rooms and rooms-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomsComponent } from './views/rooms/rooms.component';
+import { RoomsListComponent } from './components/rooms-list/rooms-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    RoomsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/rooms-list/rooms-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { RoomList } from 'src/app/views/rooms/rooms';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.css']
+})
+export class RoomsListComponent implements OnInit, OnChanges {
+  @Input() rooms: RoomList[] = [];
+
+  @Input() title: string = '';
+
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['title']) {
+      this.title = changes['title'].currentValue.toupperCase();
+    }
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
+}
